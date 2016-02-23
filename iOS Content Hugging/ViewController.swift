b978e25260d430b54191d5b8a5dbf41e85bd4813
Randall Mardus <randalljmardus@gmail.com>
2016-02-23 14:00:22 -0500

    first commit

--- iOS Content Hugging/ViewController.swift
+++ iOS Content Hugging/ViewController.swift
@@ -13,6 +13,21 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    
+    let nameLabel = UILabel()
+    nameLabel.text = "UILabel"
+    let red = UIColor(hue: 358/360, saturation: 0.6, brightness: 0.98, alpha: 1)
+    nameLabel.backgroundColor = red
+    nameLabel.translatesAutoresizingMaskIntoConstraints = false
+    view.addSubview(nameLabel)
+    
+    let nameField = UITextField()
+        nameField.placeholder = "Enter name"
+        nameField.backgroundColor = UIColor(hue: 104/360, saturation: 0.74, brightness: 0.85, alpha: 1)
+        nameField.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(nameField)
+        
+    
     }
 
     override func didReceiveMemoryWarning() {
